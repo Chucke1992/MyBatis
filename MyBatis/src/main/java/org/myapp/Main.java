acfb862e8bd092aa1b92adc337359c288178eedd
Chucke1992 <artemii1992@yahoo.com>
2014-05-19 14:29:40 +0300

    Update changeLog.xml, Main.java

--- MyBatis/src/main/java/org/myapp/Main.java
+++ MyBatis/src/main/java/org/myapp/Main.java
@@ -12,6 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 		SimpleMapper simpleService = new SimpleService();
 		List<SimpleObject> sObjects = simpleService.findAllObjects();
+		System.out.println("Database data");
 		for(int i = 0; i < sObjects.size(); i++)
 			System.out.println(sObjects.get(i).getId() + ";" + sObjects.get(i).getText());
 	}
